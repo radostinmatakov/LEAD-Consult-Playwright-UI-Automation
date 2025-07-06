a7e3fe639d80e624d3aaec819f66851cbd0aa751
Radostin Matakov <radostin.d.matakov@gmail.com>
2025-07-06 23:24:28 +0300

    Updated BasePage

--- pageObjects/basePage.js
+++ pageObjects/basePage.js
@@ -8,19 +8,6 @@ class BasePage {
     await this.page.goto(path);
     await this.logo.waitFor({ state: 'visible' });
   }
-
-  async click(selector) {
-    await this.page.locator(selector).click();
-  }
-
-  async isVisible(selector) {
-    return this.page.locator(selector).isVisible();
-  }
-
-  async containsText(selector, expected) {
-    const text = await this.page.locator(selector).textContent();
-    return text.toLowerCase().includes(expected.toLowerCase());
-  }
 }
 
 module.exports = BasePage;
